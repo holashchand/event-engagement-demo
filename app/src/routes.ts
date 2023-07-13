1ffe04f5f9912b69f757aeaa50f6d863d648757a
K Anirudh <anirudh.k@beehyv.com>
2023-07-13 15:44:23 +0530

    qr scanner page route and nav

--- app/src/routes.ts
+++ app/src/routes.ts
@@ -1,11 +1,11 @@
-import { FC } from "react";
 import ExhibitCardDetails from "./pages/ExhibitDetails";
+import HomePage from "./pages/Home";
 import FirstLogin from "./pages/Login";
 import QuestionSet from "./pages/QuestionSet";
-import ViewCertificate from "./pages/ViewCertificate";
 import Register from "./pages/Register";
+import ScanQR from "./pages/ScanQR";
 import UserHome from "./pages/UserHome";
-import HomePage from "./pages/Home";
+import ViewCertificate from "./pages/ViewCertificate";
 
 export const apiRoutes = {
   REGISTER: "/Register",
@@ -21,61 +21,68 @@ export const pageRoutes = {
 };
 
 interface Route {
-  key: string,
-  title: string,
-  path: string,
-  enabled: boolean,
-  component: FC<{}>
+  key: string;
+  title: string;
+  path: string;
+  enabled: boolean;
+  component: React.FC<{}>;
 }
 
-export const routes: Array<Route> = [
+export const routes: Route[] = [
   {
-      key: 'home-route',
-      title: 'HomePage',
-      path: '/',
-      enabled: true,
-      component: HomePage
+    key: "home-route",
+    title: "HomePage",
+    path: "/",
+    enabled: true,
+    component: HomePage,
   },
   {
-      key: 'register-route',
-      title: 'Register',
-      path: '/Register',
-      enabled: true,
-      component: Register
+    key: "register-route",
+    title: "Register",
+    path: "/Register",
+    enabled: true,
+    component: Register,
   },
   {
-      key: 'firstLogin-route',
-      title: 'FirstLogin',
-      path: '/FirstLogin',
-      enabled: true,
-      component: FirstLogin
+    key: "firstLogin-route",
+    title: "FirstLogin",
+    path: "/FirstLogin",
+    enabled: true,
+    component: FirstLogin,
   },
   {
-      key: 'userhome-route',
-      title: 'UserHome',
-      path: '/UserHome',
-      enabled: true,
-      component: UserHome
+    key: "userhome-route",
+    title: "UserHome",
+    path: "/UserHome",
+    enabled: true,
+    component: UserHome,
   },
   {
-      key: 'questionSet-route',
-      title: 'QuestionSet',
-      path: '/QuestionSet',
-      enabled: true,
-      component: QuestionSet
+    key: "questionSet-route",
+    title: "QuestionSet",
+    path: "/QuestionSet",
+    enabled: true,
+    component: QuestionSet,
   },
   {
-      key: 'exhibitCardDetails-route',
-      title: 'ExhibitCardDetails',
-      path: '/ExhibitCardDetails',
-      enabled: true,
-      component: ExhibitCardDetails
+    key: "exhibitCardDetails-route",
+    title: "ExhibitCardDetails",
+    path: "/ExhibitCardDetails",
+    enabled: true,
+    component: ExhibitCardDetails,
   },
   {
-      key: 'ViewCertificate-route',
-      title: 'ViewCertificate',
-      path: '/ViewCertificate',
-      enabled: true,
-      component: ViewCertificate
-  }
-]
+    key: "ViewCertificate-route",
+    title: "ViewCertificate",
+    path: "/ViewCertificate",
+    enabled: true,
+    component: ViewCertificate,
+  },
+  {
+    key: "ScanQR-route",
+    title: "ScanQR",
+    path: "/scan-qr",
+    enabled: true,
+    component: ScanQR,
+  },
+];
