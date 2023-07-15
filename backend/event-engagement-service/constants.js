9da19e566e221fa604cddaa22bbe5f760f239801
Holash Chand <holash.c@beehyv.com>
2023-07-15 10:33:27 +0530

    added service urls

---
+++ backend/event-engagement-service/constants.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    registryUrl: process.env.REGISTRY_URL || "http://localhost:8081",
+    credentialUrl: process.env.CREDENTIAL_URL || "http://localhost:3000",
+    credentialSchemaUrl: process.env.CREDENTIAL_SCHEMA_URL || "http://localhost:3333"
+}
