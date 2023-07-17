13b24b9a8f472aac48660b6b561479f91562ee25
Holash Chand <holash.c@beehyv.com>
2023-07-17 12:15:52 +0530

    added script to create exhibits

---
+++ scripts/exhibitService.js
@@ -0,0 +1,68 @@
+const { default: axios } = require("axios");
+
+const IDENTITY_URL = "http://localhost:3332"
+const serviceUrl = `http://localhost:8081/api/v1/Exhibit`;
+
+const saveExhibit = async (searchBy, value, payload) => {
+    const did = await generateDid("exhibit");
+    payload["did"] = `${did}`;
+    return axios.post(serviceUrl, finalEntity).then(resp => resp.data);
+};
+
+const getExhibitByKeyValue = async (key, value) => {
+    const payload = {
+        "offset": 0,
+        "limit": 1,
+        "filters": {
+          "exhibitDetails": {
+            [key]: {
+                "eq": value
+              }
+          }
+        }
+    }
+    const exhibit = await searchExhibit(payload);
+    return exhibit && exhibit?.length > 0 && exhibit[0];
+};
+
+const searchExhibit = async (payload) => {
+    return axios.post(`${serviceUrl}/search`, payload)
+    .then(results => results?.data);
+};
+
+const generateDid = async (entityName) => {
+    const payload = {
+        "content": 
+            [
+                {
+                    "alsoKnownAs": [],
+                    "services": [
+                        {
+                            "id": "IdentityHub",
+                            "type": "IdentityHub",
+                            "serviceEndpoint": {
+                                "@context": "schema.identity.foundation/hub",
+                                "@type": "UserServiceEndpoint",
+                                "instance": [
+                                    "did:test:hub.id"
+                                ]
+                            }
+                        }
+                    ],
+                    "method": `upai:${entityName}`
+                }
+            ]
+        
+    }
+    const did = await axios.post(`${IDENTITY_URL}/did/generate`, payload)
+    .then(result => {
+        return result?.data[0]?.id;
+    })
+    return did || "default";
+}
+
+
+
+module.exports = {
+    saveExhibit,
+}
---
+++ scripts/formatter.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+const { convertCSVToArray } = require('convert-csv-to-array');
+const converter = require('convert-csv-to-array');
+
+const csvFilePath='./data/data.csv';
+
+fs.readFile(csvFilePath, (err, inputD) => {
+    if (err) throw err;
+    const data = arrayofArrays(inputD.toString());
+    const rows = data.slice(1);
+    const objs = rows.map(row => {
+        return {
+            "exhibitDetails": {
+                "name": row[1],
+                "shortDescription": row[2],
+                "fullDescription": row[2],
+                "logoURL": "",
+                "videoURL": row[3],
+                "boothNumber": `${row[0]}`,
+                "organization": "",
+                "qrId": ""
+            },
+            "quizConfig": {
+                "title": "Quiz",
+                "description": "",
+                "questions": [
+                    {
+                        "question": row[4],
+                        "correctAnswer": row[9],
+                        "options": [
+                            row[5],
+                            row[6],
+                            row[7],
+                            row[8],
+                        ]
+                    },
+                    {
+                        "question": row[10],
+                        "correctAnswer": row[15],
+                        "options": [
+                            row[11],
+                            row[12],
+                            row[13],
+                            row[14],
+                        ]
+                    },
+                    {
+                        "question": row[16],
+                        "correctAnswer": row[21],
+                        "options": [
+                            row[17],
+                            row[18],
+                            row[19],
+                            row[20],
+                        ]
+                    },
+                    {
+                        "question": row[22],
+                        "correctAnswer": row[27],
+                        "options": [
+                            row[23],
+                            row[24],
+                            row[25],
+                            row[26],
+                        ]
+                    },
+                    {
+                        "question": row[28],
+                        "correctAnswer": row[33],
+                        "options": [
+                            row[29],
+                            row[30],
+                            row[31],
+                            row[32],
+                        ]
+                    }
+                ]
+            }
+        };
+    })
+    writeToFile('./data/final.json', objs);
+    writeToFile('./data/data.json', rows);
+ })
+
+
+const arrayofArrays = (data) => convertCSVToArray(data, {
+    type: 'array',
+    separator: ';', // use the separator you use in your csv (e.g. '\t', ',', ';' ...)
+  });
+
+
+function writeToFile(fileName, input) {
+    fs.writeFileSync(fileName, JSON.stringify(input));
+}
